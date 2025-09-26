d3696e0988fbd1dfc7a52454cf5874c744194634
Ahsan <ahsannadeem682@gmail.com>
2025-09-26 19:32:18 +0500

    Create Task5.cpp

---
+++ Lab05/Task5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int sumDigits(int n) {
+    if (n == 0)
+        return 0;
+    return (n % 10) + sumDigits(n / 10);
+}
+
+
+int nestedSum(int n) {
+    if (n >= 0 && n <= 9)
+        return n;
+    return nestedSum(sumDigits(n));
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Please enter a non-negative integer." << endl;
+        return 1;
+    }
+
+    int result = nestedSum(num);
+    cout << "Single-digit result: " << result << endl;
+
+    return 0;
+}
